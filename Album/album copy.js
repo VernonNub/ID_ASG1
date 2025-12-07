7caa3f42d659da81f098d893dfdfd15acc749997
VernonNub <Vernonloh6@gmail.com>
2025-12-07 21:39:45 +0800

    renaming files

---
+++ Album/album copy.js
@@ -0,0 +1,29 @@
+var albumList = document.getElementsByClassName("album-page-album")
+
+for (let i = 0; i < albumList.length; i++)
+{
+    albumList[i].addEventListener('click', function()
+    {
+        ChooseAlbum(i);
+    } ,false)
+}
+
+
+function ChooseAlbum(num)
+{
+    console.log(num)
+    const albums = ["AFlowerBookmark3", 
+                    "TheWinning", 
+                    "Lilac", 
+                    "Pieces", 
+                    "LovePoem", 
+                    "AFlowerBookmark2",
+                    "Palette", 
+                    "Chat-Shire", 
+                    "AFlowerBookmark1", 
+                    "LastFantasy", 
+                    "Real"];
+
+
+    localStorage.setItem("chosenAlbum", albums[num]);
+}
